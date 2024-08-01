1a8b10721082a726e0932d8edbe0ac91ad096339
aludayalu <pl46u3.ducklinz@gmail.com>
2024-08-01 11:40:33 +0530

    base daisyui and navbar setup with compression

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import request, redirect
+from monster import render, tokeniser, parser, Flask
+import sys, json
+
+app = Flask(__name__)
+
+daisyui="<script>"+open("public/pako.js").read()+"</script>"+"""<script>
+    function decompressGzippedString(base64String) {
+        try {
+            const binaryString = atob(base64String);
+            const len = binaryString.length;
+            const bytes = new Uint8Array(len);
+            for (let i = 0; i < len; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+            const decompressedData = pako.inflate(bytes, { to: 'string' });
+            return decompressedData;
+        } catch (err) {
+            console.error('An error occurred while decompressing the string:', err);
+            return null;
+        }
+    }
+    """+f"""
+    var daisycss="{open("public/daisyui.b64").read()}";
+    var style=document.createElement("style");
+    style.textContent=decompressGzippedString(daisycss);
+    document.head.appendChild(style);
+    </script>
+    """
+
+@app.get("/")
+def home():
+    navbar=render("navbar", locals())
+    return render("index", locals()|globals())
+
+app.run(host="0.0.0.0", port=int(sys.argv[1]))
